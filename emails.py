f589d3c1e1d48e86b50d390534bd305f7a34b5da
George Gonzalez Pichardo <160191643+georgedev9@users.noreply.github.com>
2024-09-08 15:47:42 -0700

    Add files via upload

---
+++ emails.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+
+import email.message, smtplib
+
+def send_email(message):
+    """Sends the message to the configured SMTP server."""
+    mail_server = smtplib.SMTP('localhost')
+    mail_server.send_message(message)
+    mail_server.quit()
+
+
+def generate_error_report(sender, recipient, subject, body):
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+    return message
+
